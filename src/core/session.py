699aa017b12e5f152ce7624f254f4511c7b5c0c9
D3nisPi <d3nis.p1shchik@gmail.com>
2024-11-16 18:23:54 +0300

    Created session factory

---
+++ src/core/session.py
@@ -0,0 +1,23 @@
+from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
+
+from config import settings
+
+engine = create_async_engine(
+    settings.db.url,
+    future=True,
+    echo=settings.engine.echo,
+    pool_size=settings.engine.pool_size,
+    max_overflow=settings.engine.max_overflow
+)
+
+async_session_factory = async_sessionmaker(
+    engine,
+    autoflush=False,
+    expire_on_commit=False,
+    autocommit=False
+)
+
+
+async def get_session():
+    async with async_session_factory() as session:
+        yield session
