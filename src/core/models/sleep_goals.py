98746b2778e680c2abcbf13868830bf45a8dc815
D3nisPi <d3nis.p1shchik@gmail.com>
2024-11-16 20:22:54 +0300

    Created sleep goals model

---
+++ src/core/models/sleep_goals.py
@@ -0,0 +1,22 @@
+from datetime import time
+from typing import TYPE_CHECKING
+
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+from src.core.models.base import Base
+if TYPE_CHECKING:
+    from src.core.models.users import UsersOrm
+
+
+class SleepGoalsOrm(Base):
+    __tablename__ = "sleep_goals"
+
+    user_id: Mapped[int] = mapped_column(ForeignKey("users.id",
+                                                    ondelete="CASCADE",
+                                                    onupdate="CASCADE"),
+                                         primary_key=True)
+    sleep_start: Mapped[time]
+    sleep_end: Mapped[time]
+
+    user: Mapped["UsersOrm"] = relationship(back_populates="sleep_goal")
--- src/core/models/users.py
+++ src/core/models/users.py
@@ -1,7 +1,11 @@
+from typing import TYPE_CHECKING
+
 from sqlalchemy import String
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from src.core.models.base import Base
+if TYPE_CHECKING:
+    from src.core.models.sleep_goals import SleepGoalsOrm
 
 
 class UsersOrm(Base):
@@ -12,3 +16,9 @@ class UsersOrm(Base):
     display_name: Mapped[str] = mapped_column(String(50))
     password_hash: Mapped[str] = mapped_column(String(120))
     refresh_token_id: Mapped[str | None] = mapped_column(String(32))
+
+    sleep_goal: Mapped["SleepGoalsOrm"] = relationship(
+        back_populates="user",
+        cascade="all, delete",
+        passive_deletes=True
+    )
