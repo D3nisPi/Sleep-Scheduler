797d368081916507bca59a015bc2d836d238b269
D3nisPi <d3nis.p1shchik@gmail.com>
2024-11-17 02:27:44 +0300

    Added user schema (like UsersOrm)

---
+++ src/api/schemas/users.py
@@ -0,0 +1,9 @@
+from src.api.schemas.base import BaseSchema
+
+
+class UserSchema(BaseSchema):
+    id: int
+    username: str
+    display_name: str
+    password_hash: str
+    refresh_token_id: str | None
