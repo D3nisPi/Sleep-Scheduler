3c80aac43c30b8a44ed1339fbfce32f602c5dacc
D3nisPi <d3nis.p1shchik@gmail.com>
2024-11-17 02:32:07 +0300

    Fixed auth endpoint

--- src/api/views/auth.py
+++ src/api/views/auth.py
@@ -1,22 +1,21 @@
 from fastapi import APIRouter, Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from src.api.actions.auth import create_tokens, validate_user_by_password
-from src.api.actions.users import update_user_refresh_token
+from src.api.actions.auth import authenticate_user_by_password, get_tokens_for_user
+from src.api.actions.users import update_user_refresh_token_by_id
 from src.api.schemas.auth import TokensResponse, TokensInfo
-from src.core.models.users import UsersORM
+from src.api.schemas.users import UserSchema
 from src.core.session import get_session
 
 auth_router = APIRouter(prefix='/auth', tags=["Auth"])
 
 
-@auth_router.post("/login/")
-async def login(
-    user: UsersORM = Depends(validate_user_by_password),
-    token_info: TokensInfo = Depends(create_tokens),
-    session: AsyncSession = Depends(get_session)
-) -> TokensResponse:
-    await update_user_refresh_token(user, token_info.refresh_jti, session)
+@auth_router.post("/login/", response_model=TokensResponse, status_code=201)
+async def login(user: UserSchema = Depends(authenticate_user_by_password),
+                token_info: TokensInfo = Depends(get_tokens_for_user),
+                session: AsyncSession = Depends(get_session)
+                ) -> TokensResponse:
+    await update_user_refresh_token_by_id(user.id, token_info.refresh_jti, session)
     return TokensResponse(
         access_token=token_info.access_token,
         refresh_token=token_info.refresh_token
