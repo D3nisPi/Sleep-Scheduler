8616edad7ce7e3841cb2de42c210de36da62b0cc
D3nisPi <d3nis.p1shchik@gmail.com>
2024-11-16 22:39:37 +0300

    Changed class names "*Orm" -> "*ORM"

--- src/api/dals/users.py
+++ src/api/dals/users.py
@@ -1,17 +1,17 @@
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from src.core.models.users import UsersOrm
+from src.core.models.users import UsersORM
 
 
 class UsersDAL:
     def __init__(self, session: AsyncSession):
         self.session = session
 
-    async def get_user_by_username(self, username: str) -> UsersOrm | None:
+    async def get_user_by_username(self, username: str) -> UsersORM | None:
         query = (
-            select(UsersOrm)
-            .where(UsersOrm.username == username)
+            select(UsersORM)
+            .where(UsersORM.username == username)
         )
         result = await self.session.execute(query)
         user = result.scalars().first()
