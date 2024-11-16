2543ba9eaf4745e89b134eb80f7bffb5fef1347e
D3nisPi <d3nis.p1shchik@gmail.com>
2024-11-16 19:59:31 +0300

    Changed class name "User" -> "UsersOrm"

--- src/core/models/users.py
+++ src/core/models/users.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import Mapped, mapped_column
 from src.core.models.base import Base
 
 
-class User(Base):
+class UsersOrm(Base):
     __tablename__ = "users"
 
     id: Mapped[int] = mapped_column(primary_key=True)
