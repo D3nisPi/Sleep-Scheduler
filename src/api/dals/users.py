2e2f362da7d6d44b6a43f50ff523ff8e0e0a3684
D3nisPi <d3nis.p1shchik@gmail.com>
2024-11-16 21:48:54 +0300

    Created a part of data access layer for the table “users”

---
+++ src/api/dals/users.py
@@ -0,0 +1,18 @@
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from src.core.models.users import UsersOrm
+
+
+class UserDAL:
+    def __init__(self, session: AsyncSession):
+        self.session = session
+
+    async def get_user_by_username(self, username: str) -> UsersOrm | None:
+        query = (
+            select(UsersOrm)
+            .where(UsersOrm.username == username)
+        )
+        result = await self.session.execute(query)
+        user = result.scalars().first()
+        return user
