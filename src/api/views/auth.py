3affc8020b87726ed3e4c8a9a74bf020806d0911
D3nisPi <d3nis.p1shchik@gmail.com>
2024-11-17 23:02:16 +0300

    Added "/refresh/" endpoint

--- src/api/views/auth.py
+++ src/api/views/auth.py
@@ -1,13 +1,16 @@
 from fastapi import APIRouter, Depends
+from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
 from sqlalchemy.ext.asyncio import AsyncSession
 from starlette import status
 
-from src.api.actions.auth import authenticate_user_by_password, create_tokens
+from src.api.actions.auth import authenticate_user_by_password, create_tokens, authenticate_user_by_refresh_token
 from src.api.actions.users import update_user_refresh_token_by_id
 from src.api.schemas.auth import Tokens, LoginData
 from src.core.session import get_session
 
 auth_router = APIRouter(prefix='/auth', tags=["Auth"])
+
+http_bearer = HTTPBearer()
 
 
 @auth_router.post("/login/", response_model=Tokens, status_code=status.HTTP_201_CREATED)
@@ -15,6 +18,20 @@ async def login(body: LoginData,
                 session: AsyncSession = Depends(get_session)
                 ) -> Tokens:
     user = await authenticate_user_by_password(body, session)
+    tokens_info = create_tokens(user)
+    await update_user_refresh_token_by_id(user.id, tokens_info.refresh_token_id, session)
+    return Tokens(
+        access_token=tokens_info.access_token,
+        refresh_token=tokens_info.refresh_token
+    )
+
+
+@auth_router.post("/refresh/", response_model=Tokens, status_code=status.HTTP_201_CREATED)
+async def refresh_token(credentials: HTTPAuthorizationCredentials = Depends(http_bearer),
+                        session: AsyncSession = Depends(get_session)
+                        ) -> Tokens:
+    token = credentials.credentials
+    user = await authenticate_user_by_refresh_token(token, session)
     tokens_info = create_tokens(user)
     await update_user_refresh_token_by_id(user.id, tokens_info.refresh_token_id, session)
     return Tokens(
