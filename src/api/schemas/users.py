ca071a020b35f474345a07d5f461af11685a30a3
D3nisPi <d3nis.p1shchik@gmail.com>
2024-11-17 23:42:44 +0300

    Pydantic model of GET response for users

--- src/api/schemas/users.py
+++ src/api/schemas/users.py
@@ -7,3 +7,10 @@ class UserSchema(BaseSchema):
     display_name: str
     password_hash: str
     refresh_token_id: str | None
+
+
+class UserReadResponse(BaseSchema):
+    id: int
+    username: str
+    display_name: str
+
