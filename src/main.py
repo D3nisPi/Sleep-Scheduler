0979d428a05032ed0e37d24dd7dea8a0922cb034
D3nisPi <d3nis.p1shchik@gmail.com>
2024-11-17 23:43:25 +0300

    Added users router

--- src/main.py
+++ src/main.py
@@ -2,11 +2,13 @@ import uvicorn
 from fastapi import FastAPI
 
 from src.api.views.auth import auth_router
+from src.api.views.users import users_router
 from src.core.config import settings
 
 
 app = FastAPI()
 app.include_router(auth_router)
+app.include_router(users_router)
 
 if __name__ == "__main__":
     uvicorn.run("main:app", reload=True, host=settings.run.host, port=settings.run.port)
