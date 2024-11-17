764d9a849e08e8894a27bf305272876d32fa31ac
D3nisPi <d3nis.p1shchik@gmail.com>
2024-11-17 23:00:44 +0300

    Added refresh token decoding

--- src/api/utils/tokens.py
+++ src/api/utils/tokens.py
@@ -1,6 +1,10 @@
 from datetime import datetime, timedelta
 
 import jwt
+from fastapi import HTTPException
+from starlette import status
+
+from src.api.schemas.auth import RefreshTokenPayload
 from src.core.config import settings
 
 
@@ -30,3 +34,37 @@ def create_refresh_token(user_id: int,
         "type": "refresh"
     }
     return jwt.encode(payload, settings.jwt.secret_key, settings.jwt.algorithm)
+
+
+def decode_token(token: str) -> dict:
+    invalid_token = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Invalid token"
+    )
+    malformed_token = HTTPException(
+        status_code=status.HTTP_400_BAD_REQUEST,
+        detail="Malformed token"
+    )
+
+    try:
+        payload = jwt.decode(token, settings.jwt.secret_key, algorithms=[settings.jwt.algorithm])
+    except jwt.InvalidSignatureError:
+        raise invalid_token
+    except jwt.DecodeError as e:
+        print(e)
+        raise malformed_token
+    except jwt.InvalidTokenError:
+        raise invalid_token
+
+    return payload
+
+
+def decode_refresh_token(token: str) -> RefreshTokenPayload:
+    payload = RefreshTokenPayload(**decode_token(token))
+    if payload.type != "refresh":
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Invalid token type. Expected: 'refresh'"
+        )
+
+    return payload
