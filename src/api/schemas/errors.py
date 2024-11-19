bc72eac5eae6e8d465fdd70e65d773159537a41c
D3nisPi <d3nis.p1shchik@gmail.com>
2024-11-19 23:56:58 +0300

    Pydantic model of database errors

---
+++ src/api/schemas/errors.py
@@ -0,0 +1,19 @@
+from typing import List, Literal
+
+from src.api.schemas.base import BaseSchema
+
+
+class DatabaseErrorDetail(BaseSchema):
+    table: str
+    fields: list[str]
+    error_type: Literal[
+        "unique_constraint",
+        "check_constraint",
+        "primary_key_constraint",
+        "foreign_key_constraint",
+        "unknown"
+    ]
+
+
+class DatabaseErrorResponse(BaseSchema):
+    detail: List[DatabaseErrorDetail]
