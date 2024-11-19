1a2b7b50b90d2171ec7e0f046f4fd6df6631d2f6
D3nisPi <d3nis.p1shchik@gmail.com>
2024-11-20 01:36:54 +0300

    Better info for /docs

--- src/api/views/__init__.py
+++ src/api/views/__init__.py
@@ -1,18 +1,17 @@
 from fastapi import HTTPException
 from fastapi.security import HTTPBearer
-from starlette import status
+from starlette.status import (
+    HTTP_404_NOT_FOUND,
+    HTTP_400_BAD_REQUEST
+)
 
 http_bearer = HTTPBearer()
 
 user_not_found = HTTPException(
-    status_code=status.HTTP_404_NOT_FOUND,
+    status_code=HTTP_404_NOT_FOUND,
     detail=f"User not found"
 )
-database_conflict = HTTPException(
-    status_code=status.HTTP_409_CONFLICT,
-    detail=f"Database conflict"
-)
 no_parameters = HTTPException(
-    status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+    status_code=HTTP_400_BAD_REQUEST,
     detail="At least one specified parameter for user update should be provided",
 )
--- src/api/views/users.py
+++ src/api/views/users.py
@@ -1,19 +1,52 @@
 from fastapi import APIRouter, Depends, Response
 from fastapi.security import HTTPAuthorizationCredentials
-from sqlalchemy.exc import IntegrityError
 from sqlalchemy.ext.asyncio import AsyncSession
-from starlette import status
+from starlette.status import (
+    HTTP_200_OK,
+    HTTP_201_CREATED,
+    HTTP_400_BAD_REQUEST,
+    HTTP_404_NOT_FOUND,
+    HTTP_409_CONFLICT,
+    HTTP_503_SERVICE_UNAVAILABLE
+)
 
 from src.api.actions.users import get_user_by_id, create_new_user, delete_user_by_id, update_user_by_id
+from src.api.schemas.errors import DatabaseErrorResponse, CommonErrorResponse
 from src.api.schemas.users import UserReadResponse, UserCreateRequest, UserUpdateRequest
 from src.api.utils.tokens import decode_access_token
-from src.api.views import http_bearer, user_not_found, database_conflict, no_parameters
+from src.api.views import http_bearer, user_not_found, no_parameters
 from src.core.session import get_session
 
 users_router = APIRouter(prefix='/users', tags=["Users"])
 
+get_user_responses = {
+    HTTP_200_OK: {"model": UserReadResponse, "description": "Successful Response"},
+    HTTP_404_NOT_FOUND: {"model": CommonErrorResponse, "description": "User not found"},
+    HTTP_503_SERVICE_UNAVAILABLE: {"model": CommonErrorResponse, "description": "Database connection problems"}
+}
 
-@users_router.get("/", response_model=UserReadResponse, status_code=status.HTTP_200_OK)
+create_user_responses = {
+    HTTP_201_CREATED: {"description": "Successful Response"},
+    HTTP_409_CONFLICT: {"model": DatabaseErrorResponse, "description": "Database conflict"},
+    HTTP_503_SERVICE_UNAVAILABLE: {"model": CommonErrorResponse, "description": "Database connection problems"}
+}
+
+delete_user_responses = {
+    HTTP_200_OK: {"description": "Successful Response"},
+    HTTP_404_NOT_FOUND: {"model": CommonErrorResponse, "description": "User not found"},
+    HTTP_503_SERVICE_UNAVAILABLE: {"model": CommonErrorResponse, "description": "Database connection problems"}
+}
+
+update_user_responses = {
+    HTTP_200_OK: {"description": "Successful Response"},
+    HTTP_400_BAD_REQUEST: {"model": CommonErrorResponse, "description": "No parameters to update were given"},
+    HTTP_404_NOT_FOUND: {"model": CommonErrorResponse, "description": "User not found"},
+    HTTP_409_CONFLICT: {"model": DatabaseErrorResponse, "description": "Database conflict"},
+    HTTP_503_SERVICE_UNAVAILABLE: {"model": CommonErrorResponse, "description": "Database connection problems"}
+}
+
+
+@users_router.get("/", status_code=HTTP_200_OK, responses=get_user_responses)
 async def get_user(credentials: HTTPAuthorizationCredentials = Depends(http_bearer),
                    session: AsyncSession = Depends(get_session)
                    ) -> UserReadResponse:
@@ -26,19 +59,15 @@ async def get_user(credentials: HTTPAuthorizationCredentials = Depends(http_bear
     return UserReadResponse.model_validate(user)
 
 
-@users_router.post("/")
+@users_router.post("/", status_code=HTTP_201_CREATED, response_class=Response, responses=create_user_responses)
 async def create_user(body: UserCreateRequest,
                       session: AsyncSession = Depends(get_session)
                       ) -> Response:
-    try:
-        await create_new_user(body, session)
-    except IntegrityError:
-        raise database_conflict
-
-    return Response(status_code=status.HTTP_201_CREATED)
+    await create_new_user(body, session)
+    return Response(status_code=HTTP_201_CREATED)
 
 
-@users_router.delete("/")
+@users_router.delete("/", status_code=HTTP_200_OK, response_class=Response, responses=delete_user_responses)
 async def delete_user(credentials: HTTPAuthorizationCredentials = Depends(http_bearer),
                       session: AsyncSession = Depends(get_session)
                       ) -> Response:
@@ -48,10 +77,10 @@ async def delete_user(credentials: HTTPAuthorizationCredentials = Depends(http_b
     if not deleted:
         raise user_not_found
 
-    return Response(status_code=status.HTTP_200_OK)
+    return Response(status_code=HTTP_200_OK)
 
 
-@users_router.patch("/")
+@users_router.patch("/", status_code=HTTP_200_OK, response_class=Response, responses=update_user_responses)
 async def update_user(body: UserUpdateRequest,
                       credentials: HTTPAuthorizationCredentials = Depends(http_bearer),
                       session: AsyncSession = Depends(get_session)
@@ -61,11 +90,9 @@ async def update_user(body: UserUpdateRequest,
     updated_user_params = body.model_dump(exclude_none=True)
     if not updated_user_params:
         raise no_parameters
-    try:
-        updated = await update_user_by_id(payload.sub, updated_user_params, session)
-        if not updated:
-            raise user_not_found
-    except IntegrityError:
-        raise database_conflict
 
-    return Response(status_code=status.HTTP_200_OK)
+    updated = await update_user_by_id(payload.sub, updated_user_params, session)
+    if not updated:
+        raise user_not_found
+
+    return Response(status_code=HTTP_200_OK)
