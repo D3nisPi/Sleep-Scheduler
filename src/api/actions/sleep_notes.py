adbdfa511fcf4a832f693ab88b670cd7a8f59269
D3nisPi <d3nis.p1shchik@gmail.com>
2024-11-20 19:03:32 +0300

    CRUD for "sleep_notes" table

---
+++ src/api/actions/sleep_notes.py
@@ -0,0 +1,79 @@
+from datetime import date
+
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from src.api.dals.sleep_notes import SleepNoteDAL
+from src.api.schemas.sleep_notes import SleepNoteSchema, SleepNoteCreateRequest
+
+
+async def get_sleep_note_by_id_and_user_id(note_id: int,
+                                           user_id: int,
+                                           session: AsyncSession
+                                           ) -> SleepNoteSchema | None:
+    async with session.begin():
+        sleep_note_dal = SleepNoteDAL(session)
+        sleep_note = await sleep_note_dal.get_sleep_note_by_id_and_user_id(note_id, user_id)
+        if sleep_note is None:
+            return
+        return SleepNoteSchema.model_validate(sleep_note)
+
+
+async def get_sleep_note_by_date_and_user_id(note_date: date,
+                                             user_id: int,
+                                             session: AsyncSession
+                                             ) -> SleepNoteSchema | None:
+    async with session.begin():
+        sleep_note_dal = SleepNoteDAL(session)
+        sleep_note = await sleep_note_dal.get_sleep_note_by_date_and_user_id(note_date, user_id)
+        if sleep_note is None:
+            return
+        return SleepNoteSchema.model_validate(sleep_note)
+
+
+async def create_new_sleep_note(user_id, body: SleepNoteCreateRequest, session: AsyncSession) -> None:
+    async with session.begin():
+        sleep_note_dal = SleepNoteDAL(session)
+        await sleep_note_dal.create_sleep_note(
+            body.note_date,
+            body.sleep_start,
+            body.sleep_end,
+            body.rating,
+            body.comment,
+            user_id
+        )
+
+
+async def delete_sleep_note_by_id_and_user_id(note_id: int, user_id: int, session: AsyncSession) -> bool:
+    async with session.begin():
+        sleep_note_dal = SleepNoteDAL(session)
+        deleted = await sleep_note_dal.delete_sleep_note_by_id_and_user_id(note_id, user_id)
+        return deleted
+
+
+async def delete_sleep_note_by_date_and_user_id(note_date: date, user_id: int, session: AsyncSession) -> bool:
+    async with session.begin():
+        sleep_note_dal = SleepNoteDAL(session)
+        deleted = await sleep_note_dal.delete_sleep_note_by_date_and_user_id(note_date, user_id)
+        return deleted
+
+
+async def update_sleep_note_by_id_and_user_id(note_id: int,
+                                              user_id: int,
+                                              updated_params: dict,
+                                              session: AsyncSession
+                                              ) -> bool:
+    async with session.begin():
+        sleep_note_dal = SleepNoteDAL(session)
+        updated = await sleep_note_dal.update_sleep_note_by_id_and_user_id(note_id, user_id, **updated_params)
+        return updated
+
+
+async def update_sleep_note_by_date_and_user_id(note_date: date,
+                                                user_id: int,
+                                                updated_params: dict,
+                                                session: AsyncSession
+                                                ) -> bool:
+    async with session.begin():
+        sleep_note_dal = SleepNoteDAL(session)
+        updated = await sleep_note_dal.update_sleep_note_by_date_and_user_id(note_date, user_id, **updated_params)
+        return updated
---
+++ src/api/dals/sleep_notes.py
@@ -0,0 +1,92 @@
+from datetime import date, time
+
+from sqlalchemy import select, delete, update, and_
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from src.core.models.sleep_notes import SleepNotesORM
+
+
+class SleepNoteDAL:
+    def __init__(self, session: AsyncSession):
+        self.session = session
+
+    async def get_sleep_note_by_id_and_user_id(self, note_id: int, user_id: int) -> SleepNotesORM | None:
+        query = (
+            select(SleepNotesORM)
+            .where(and_(SleepNotesORM.id == note_id, SleepNotesORM.user_id == user_id))
+        )
+        result = await self.session.execute(query)
+        sleep_note = result.scalars().first()
+        return sleep_note
+
+    async def get_sleep_note_by_date_and_user_id(self, note_date: date, user_id: int) -> SleepNotesORM | None:
+        query = (
+            select(SleepNotesORM)
+            .where(and_(SleepNotesORM.note_date == note_date, SleepNotesORM.user_id == user_id))
+        )
+        result = await self.session.execute(query)
+        sleep_note = result.scalars().first()
+        return sleep_note
+
+    async def create_sleep_note(self,
+                                note_date: date,
+                                sleep_start: time,
+                                sleep_end: time,
+                                rating: int | None,
+                                comment: str | None,
+                                user_id: int
+                                ) -> None:
+        new_sleep_note = SleepNotesORM(
+            note_date=note_date,
+            sleep_start=sleep_start,
+            sleep_end=sleep_end,
+            rating=rating,
+            comment=comment,
+            user_id=user_id
+        )
+        self.session.add(new_sleep_note)
+        await self.session.commit()
+
+    async def delete_sleep_note_by_id_and_user_id(self, note_id: int, user_id: int) -> bool:
+        query = (
+            delete(SleepNotesORM)
+            .where(and_(SleepNotesORM.id == note_id, SleepNotesORM.user_id == user_id))
+            .returning(SleepNotesORM.user_id)
+        )
+        res = await self.session.execute(query)
+        await self.session.commit()
+
+        return bool(res.scalars().first())
+
+    async def delete_sleep_note_by_date_and_user_id(self, note_date: date, user_id: int) -> bool:
+        query = (
+            delete(SleepNotesORM)
+            .where(and_(SleepNotesORM.note_date == note_date, SleepNotesORM.user_id == user_id))
+            .returning(SleepNotesORM.user_id)
+        )
+        res = await self.session.execute(query)
+        await self.session.commit()
+
+        return bool(res.scalars().first())
+
+    async def update_sleep_note_by_id_and_user_id(self, note_id: int, user_id: int, **updated_params) -> bool:
+        query = (
+            update(SleepNotesORM)
+            .where(and_(SleepNotesORM.id == note_id, SleepNotesORM.user_id == user_id))
+            .values(updated_params)
+            .returning(SleepNotesORM.user_id)
+        )
+        res = await self.session.execute(query)
+        await self.session.commit()
+        return bool(res.scalars().first())
+
+    async def update_sleep_note_by_date_and_user_id(self, nt_date: date, user_id: int, **updated_params) -> bool:
+        query = (
+            update(SleepNotesORM)
+            .where(and_(SleepNotesORM.note_date == nt_date, SleepNotesORM.user_id == user_id))
+            .values(updated_params)
+            .returning(SleepNotesORM.user_id)
+        )
+        res = await self.session.execute(query)
+        await self.session.commit()
+        return bool(res.scalars().first())
